6cf2cd81c6470ad4737903238f02fcd6cd70deb0
hhzxxx <hhh710421059@gmail.com>
2024-12-03 16:45:11 +0800

    123

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
 import nodePositions from '$lib/data/nodes.json';
-import nodeData from '$lib/data/nodes_desc.json';
+import nodeData from '$lib/data/nodes_desc_zh.json';
 
 interface NodeDataJSON {
 	[nodeID: string]: {
